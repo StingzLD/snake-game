a217568bc7119dff82383d9f395b8265a55c73a7
Corey Hastings <stingzld@gmail.com>
2022-12-30 19:22:13 -0600

    Converted starting coordinates and move distance to constants

--- snake.py
+++ snake.py
@@ -1,24 +1,28 @@
 import turtle as t
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 
-class Snake():
+
+class Snake:
     def __init__(self):
         self.segments = []
+        self.create_snake()
 
-        # Create starting segments
-        for n in range(3):
+    def create_snake(self):
+        # Create the first three segments of the snake
+        for position in STARTING_POSITIONS:
             new_segment = t.Turtle(shape="square")
             new_segment.color("white")
             new_segment.penup()
-            new_segment.setposition(-20 * n, 0)
+            new_segment.setposition(position)
             self.segments.append(new_segment)
 
     def move(self):
-        # Move snake by making each segment copy the segment in front of its
-        # position, then move the head according to the user's input
+        # Make each segment copy the segment in front of its position
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].setposition(new_x, new_y)
-
-        self.segments[0].forward(20)
+        # Move head of snake
+        self.segments[0].forward(MOVE_DISTANCE)
