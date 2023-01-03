c86e09bc8c56dc494e7c8531513ad28ef259b120
Corey Hastings <stingzld@gmail.com>
2023-01-03 12:58:16 -0600

    Added persistence to high score between sessions

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,11 @@
 from turtle import Turtle
 
 
+def get_high_score():
+    with open("high_score.txt", "r") as file:
+        return int(file.read())
+
+
 class Scoreboard(Turtle):
     def __init__(self, screen_width, screen_length):
         super().__init__()
@@ -12,7 +17,7 @@ class Scoreboard(Turtle):
         self.setposition(self.STARTING_POSITION)
         self.color("white")
         self.score = 0
-        self.high_score = 0
+        self.high_score = get_high_score()
 
     def display_score(self):
         self.clear()
@@ -31,9 +36,14 @@ class Scoreboard(Turtle):
                    font=('Courier', 20, 'bold'))
         self.check_high_score()
 
+    def update_high_score(self):
+        self.high_score = self.score
+        with open("high_score.txt", "w") as file:
+            file.write(str(self.high_score))
+
     def check_high_score(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            self.update_high_score()
             self.setposition(x=0, y=60)
             self.write("NEW HIGH SCORE!",
                        align="center",
